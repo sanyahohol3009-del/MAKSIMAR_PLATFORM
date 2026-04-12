d1171939b2bb8ee1dcabf1815f2ea83020f0ad33
sanyahohol3009-del <sanyahohol3009@gmail.com>
2026-04-12 03:59:52 +0200

    Remove export surfaces and final visual contracts from oob dashboard branch

--- MAKSIMAR_CORE_LIB/oob_dashboard/__init__.py
+++
@@ -1,4 +0,0 @@
-from MAKSIMAR_CORE_LIB.oob_dashboard.exports_display import *
-from MAKSIMAR_CORE_LIB.oob_dashboard.exports_foundation import *
-from MAKSIMAR_CORE_LIB.oob_dashboard.exports_operator import *
-from MAKSIMAR_CORE_LIB.oob_dashboard.exports_other import *
--- MAKSIMAR_CORE_LIB/oob_dashboard/exports_other.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-"""
-Exports that are intentionally preserved but not yet classified into a canonical group.
-"""
-
-__all__ = []
